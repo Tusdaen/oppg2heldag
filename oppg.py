85610c78132c27df2ea4335869de9df260ec260c
Fridalyssand <fridalyssand@outlook.com>
2022-12-06 13:15:03 +0100

    versjon frida

--- oppg.py
+++ oppg.py
@@ -1,15 +1,32 @@
-import json
+#import json
 
-filnavn = "sivilstand.json"
+#filnavn = "sivilstand.json"
 
-with open(filnavn, encoding="utf-8") as fil:
-  data = json.load(fil)
+#with open(filnavn, encoding="utf-8") as fil:
+#  data = json.load(fil)
 
-print(data)
+#print(data)
 
 import csv
+import matplotlib.pyplot as plt
 filnavnEn = "Befolkning.csv"
+
+aarstall = []
+befolkning = []
 
 with open(filnavnEn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
-    
+
+    overskrifter = next(filinnhold)
+    print(overskrifter)
+
+    for rad in filinnhold:
+      aarstall.append(rad[0])
+      befolkning.append(rad[1])
+
+plt.plot(aarstall, befolkning)
+plt.title("Befolkningsvekst fra 1769 til 2022")
+plt.show
+
+
+
