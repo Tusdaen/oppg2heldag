399aa255edd0ead793f930a8900f19a7f81bc1d1
helena-asvg <e-helhaa3@iskule.no>
2022-12-06 13:49:28 +0100

    Oppgave 2

--- oppg.py
+++ oppg.py
@@ -1,15 +1,48 @@
 import json
+import matplotlib.pyplot as plt
 
-filnavn = "sivilstand.json"
+filnavn = "Siviltilstand.json"
 
 with open(filnavn, encoding="utf-8") as fil:
   data = json.load(fil)
 
-print(data)
+aarstallListe = []
+for aarstall in data["dataset"]["dimension"]["Tid"]["category"]["index"]:
+    aarstallListe.append((aarstall))
+#print(aarstallListe)
+
+ugift = []
+gift = []
+enke = []
+separert = []
+skilt = []
+
+for i in range(42):
+    ugift.append(data["dataset"]["value"][i])
+
+for i in range(42,84):
+    gift.append(data["dataset"]["value"][i])
+
+for i in range(84,126):
+    enke.append(data["dataset"]["value"][i])
+
+for i in range(126,168):
+    separert.append(data["dataset"]["value"][i])
+
+for i in range(168,210):
+    skilt.append(data["dataset"]["value"][i])
+
+plt.plot(aarstallListe, ugift, label="Ugift")
+plt.plot(aarstallListe, gift, label="Gift")
+plt.plot(aarstallListe, enke, label="Enke/enkemann")
+plt.plot(aarstallListe, separert, label="Separert")
+plt.plot(aarstallListe, skilt, label="Skilt")
+plt.xticks(rotation=45)
+plt.legend()
+plt.show()
 
 import csv
 filnavnEn = "Befolkning.csv"
 
 with open(filnavnEn, encoding="utf-8-sig") as fil:
     filinnhold = csv.reader(fil, delimiter=";")
-    
